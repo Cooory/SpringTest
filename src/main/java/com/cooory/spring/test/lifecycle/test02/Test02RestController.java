a07d1ea7817c73497d32f91860ae6b6c82d52c10
Cory <cory1459@gmail.com>
2023-08-22 13:22:30 +0900

    0821

---
+++ src/main/java/com/cooory/spring/test/lifecycle/test02/Test02RestController.java
@@ -0,0 +1,16 @@
+package com.cooory.spring.test.lifecycle.test02;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/lifecycle/test02")
+public class Test02RestController {
+
+//	@RequestMapping("/3")
+	
+//	public Board objectResponse() {
+//		Board Board1 = new Board();
+//	}
+	
+}
