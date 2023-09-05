757b1b5818709d945d5d85750297c128b5ed4e8a
Cory <cory1459@gmail.com>
2023-09-05 16:58:32 +0900

    0905(1)

---
+++ src/main/java/com/cooory/spring/test/jstl/repository/WeatherRepository.java
@@ -0,0 +1,25 @@
+package com.cooory.spring.test.jstl.repository;
+
+import java.util.Date;
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
+
+import com.cooory.spring.test.jstl.domain.Weather;
+
+@Repository
+public interface WeatherRepository {
+
+	public List<Weather> selectWeatherHistory();
+	
+	public int insertWeather(
+			@Param("date") Date date
+			, @Param("weather") String weather
+			, @Param("temperatures") double temperatures
+			, @Param("precipitation") double precipitation
+			, @Param("microDust") String microDust
+			, @Param("windSpeed") double windSpeed);
+	
+	public int insertWeatherByObject(Weather weather);
+}
---
+++ src/main/java/com/cooory/spring/test/jstl/service/WeatherService.java
@@ -0,0 +1,44 @@
+package com.cooory.spring.test.jstl.service;
+
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cooory.spring.test.jstl.domain.Weather;
+import com.cooory.spring.test.jstl.repository.WeatherRepository;
+
+@Service
+public class WeatherService {
+	
+	@Autowired
+	private WeatherRepository weatherRepository;
+	
+	public List<Weather> getWeatherHistory() {
+		List<Weather> weatherhistory = weatherRepository.selectWeatherHistory();
+		
+		return weatherhistory;
+	}
+	
+	public int addWeather(
+			Date date
+			, String weather
+			, double temperatures
+			, double precipitation
+			, String microDust
+			, double windSpeed) {
+		
+		int count = weatherRepository.insertWeather(date, weather, temperatures, precipitation, microDust, windSpeed);
+		
+		
+		return count;
+	}
+
+	
+	public int addWeatherByObject(Weather weather) {
+		int count = weatherRepository.insertWeatherByObject(weather);
+		
+		return count;
+	}
+}
